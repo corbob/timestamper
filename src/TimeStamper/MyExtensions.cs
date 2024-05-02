a297253621e0b9c9ebabbeff725807305322330c
Cory Knox <me@coryknox.dev>
2024-05-02 13:31:06 -0700

    (#16)(#12)(#15) Create AOT binary
    This commit adds an AOT compiled binary. It also adds the initial
    configuration needed to hopefully store the configuration in a file to
    allow customization of the configuration.
    
    Adds a link back to the releases page to VERIFICATION.txt

---
+++ src/TimeStamper/MyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace TimeStamper
+{
+    public static class MyExtensions
+    {
+        public static void PrintLine(this TextWriter output, string line, string colorEscape, string timeFormat)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                return;
+            }
+
+            output.WriteLine(DateTime.Now.ToString(timeFormat).Colorize(colorEscape) + line);
+        }
+
+        public static string Colorize(this string input, string colorEscape)
+        {
+            return $"\x1b[{colorEscape}m{input}\x1b[0m";
+        }
+    }
+}
